ce8989704aa5fdec6d534451b9239384d8478731
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-09-12 12:52:30 +0530

    Adds Hello world in go

---
+++ ch1/hello-world/hello-world_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	expected := "Hello World, learn Go the hard way"
+
+	actual := hello()
+	if expected != actual {
+		t.Fatalf("expected %v got %v", expected, actual)
+	}
+}
